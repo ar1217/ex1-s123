6ec9d52de04c71306757f4c8309d4bd35f753be5
Andrea Rubino <andre.utnfrt@gmail.com>
2022-09-01 12:52:23 -0300

    added button to change state

--- src/components/pure/componenteB.jsx
+++ src/components/pure/componenteB.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Contacto } from '../../models/Contacto.class.js';
+import { useState } from 'react';
+
 
 const ComponenteB = ({ contacto }) => {
+    const [conectado, setConectado] = useState(contacto.conectado);
     
     return (
         // renderizar en A
@@ -10,7 +13,8 @@ const ComponenteB = ({ contacto }) => {
             <h3>Nombre: { contacto.nombre } </h3>
             <h3>Apellido: { contacto.apellido } </h3>
             <h3>Email: { contacto.email } </h3>
-            <h3>Conectado: { contacto.conectado ? 'Contacto En Línea' : 'Contacto No Disponible' } </h3>
+            <h3>Conectado: { conectado ? 'Contacto En Línea' : 'Contacto No Disponible' } </h3>
+            <button onClick={() => setConectado(!conectado)}> {conectado ? 'Conectado' : 'Desconectado'}</button>
         </div>
     );
 };
